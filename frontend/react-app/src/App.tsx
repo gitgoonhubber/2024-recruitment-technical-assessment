f1c5d5df5dc973025304e8d60246001f7b74032b
Keven Liu <kevenliu2@gmail.com>
2024-02-21 13:14:00 +1100

    react-app

---
+++ frontend/react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
+
+export default App;
