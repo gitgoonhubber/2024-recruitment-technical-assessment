a1901ecba899b92912e0dce21b5fd069cd0a44da
Keven Liu <kevenliu2@gmail.com>
2024-02-22 09:08:34 +1100

    Add information container and style

---
+++ frontend/react-app/src/components/Info.tsx
@@ -0,0 +1,12 @@
+import Description from "./Description";
+import "./Info.css";
+
+const Info = () => {
+  return (
+    <div className="info-container">
+      <Description/>
+    </div>
+  );
+};
+
+export default Info;
