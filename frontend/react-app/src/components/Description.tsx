6d7fff4cb7a3e06772da7dd99e379f7f61329bc6
Keven Liu <kevenliu2@gmail.com>
2024-02-22 09:07:25 +1100

    Add description

---
+++ frontend/react-app/src/components/Description.tsx
@@ -0,0 +1,15 @@
+import "./Description.css";
+
+const Description = () => {
+  return (
+    <div className="description-container">
+      <p className="author">DevSoc presents</p>
+      <h1>unielectives</h1>
+      <p className="description">
+        Your one-stop shop for UNSW course and elective reviews
+      </p>
+    </div>
+  );
+};
+
+export default Description;
