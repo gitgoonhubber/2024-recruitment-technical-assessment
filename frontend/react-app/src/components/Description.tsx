ec5fccf4faa880b36e9b7c3b7fdcd79df3cfe569
Keven Liu <kevenliu2@gmail.com>
2024-02-24 00:56:19 +1100

    Add description and style

--- frontend/react-app/src/components/Description.tsx
+++ frontend/react-app/src/components/Description.tsx
@@ -1,10 +1,21 @@
 import "./Description.css";
+import { useState } from "react";
 
 const Description = () => {
+  const [colour, setColour] = useState("#1279F2");
+
+  const handleClick = () => {
+    const randomColour =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setColour(randomColour);
+  };
+
   return (
     <div className="description-container">
       <p className="author">DevSoc presents</p>
-      <h1>unielectives</h1>
+      <h1 style={{ color: colour }} onClick={handleClick}>
+        unilectives
+      </h1>
       <p className="description">
         Your one-stop shop for UNSW course and elective reviews
       </p>
