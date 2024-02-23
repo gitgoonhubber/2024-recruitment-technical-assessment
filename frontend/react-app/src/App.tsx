4f36c7f92a8122017219661a42bcf52caac1f031
Keven Liu <kevenliu2@gmail.com>
2024-02-24 01:00:12 +1100

    react-app

--- frontend/react-app/src/App.tsx
+++ frontend/react-app/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Info from "./components/Info";
 
 function App() {
   return (
-    <>
+    <div className="page-container">
       <Sidebar />
-    </>
+      <Info />
+    </div>
   );
 }
 
