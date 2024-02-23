e1202acf1cc2ed69fe4c048768d358f815d65243
Keven Liu <kevenliu2@gmail.com>
2024-02-24 00:57:33 +1100

    Add info style

--- frontend/react-app/src/components/Info.tsx
+++ frontend/react-app/src/components/Info.tsx
@@ -1,10 +1,18 @@
+import Cards from "./Cards";
 import Description from "./Description";
+import Dropdown from "./Dropdown";
 import "./Info.css";
+import Input from "./Input";
 
 const Info = () => {
   return (
     <div className="info-container">
-      <Description/>
+      <Description />
+      <div className="course-container">
+        <Input />
+        <Dropdown />
+        <Cards />
+      </div>
     </div>
   );
 };
